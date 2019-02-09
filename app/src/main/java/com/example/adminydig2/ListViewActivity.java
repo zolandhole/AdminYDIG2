0c6976a2ada335557925665c5b29fe0f5220847f
zolandhole <zolandhole@gmail.com>
2019-02-09 23:12:55 +0700

    Alhamdulillah

--- app/src/main/java/com/example/adminydig2/ListViewActivity.java
+++ app/src/main/java/com/example/adminydig2/ListViewActivity.java
@@ -41,6 +41,7 @@ public class ListViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+
         recyclerView = findViewById(R.id.recycler);
         textViewNophoto = findViewById(R.id.textViewNophoto);
         progressDialog = new ProgressDialog(this , R.style.MyAlertDialogStyle);
